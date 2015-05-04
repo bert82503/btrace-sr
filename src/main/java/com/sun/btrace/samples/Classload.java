12ae506717b17c65feb351a63b0226a13589c831
Bert Lee <bert825_work@163.com>
2015-05-05 01:44:50 +0800

    [Sample] Classload.java：当"一个类"被"用户定义的类加载器"加载时，则打印"栈踪迹"信息（Threads.jstack()）

--- src/main/java/com/sun/btrace/samples/Classload.java
+++ src/main/java/com/sun/btrace/samples/Classload.java
@@ -25,25 +25,34 @@
 
 package com.sun.btrace.samples;
 
+// import statics from BTraceUtils class (导入BTraceUtils类的所有静态可视成员)
 import static com.sun.btrace.BTraceUtils.*;
+// import all BTrace annotations (导入所有的BTrace注解)
 import com.sun.btrace.annotations.*;
 
 /**
+ * <p>
+ *     当"一个类"被"用户定义的类加载器"加载时，则打印"栈踪迹"信息。
+ *     在"ClassLoader.defineClass()"方法里插入一个"返回的探测点"，
+ *     以便检测类被成功加载了。
+ * </p>
  * A simple BTrace program that prints stack trace
  * whenever a class is loaded by a user-defined
  * class loader. We insert a return point probe in
  * ClassLoader.defineClass method to detect successful
  * class load.
  */
-@BTrace public class Classload {
-   @OnMethod(
-     clazz="+java.lang.ClassLoader", 
-     method="defineClass",
-     location=@Location(Kind.RETURN)
-   )   
-   public static void defineclass(@Return Class cl) {
-       println(Strings.strcat("loaded ", Reflective.name(cl)));
-       Threads.jstack();
-       println("==========================");
-   }
+@BTrace // 标识一个BTrace类
+public class Classload {
+
+    @OnMethod( // 指定类方法中的一个"探测点" (追踪行为方法)
+            clazz = "+java.lang.ClassLoader", // 待探测/追踪的类名
+            method = "defineClass", // 待探测/追踪的方法名
+            location = @Location(Kind.RETURN)) // 标识精确的"位置"或感兴趣的"探测点"，以便在方法集中进行探测
+    public static void defineclass(@Return Class cl) { // 包含"方法的返回值"信息
+        println(Strings.strcat("loaded ", Reflective.name(cl))); // 返回给定类对象的名称
+        Threads.jstack(); // 打印当前线程的"Java栈踪迹"信息
+        println("==========================");
+    }
+
 }

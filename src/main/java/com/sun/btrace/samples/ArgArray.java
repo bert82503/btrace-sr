47004e6a1ec4872d537934e0c2443ebab609cbc6
Edward Lee <bert825_work@163.com>
2015-05-30 21:13:28 +0800

    [Sample] ArgArray.java：演示“正则表达式探测点匹配”和获取输入参数作为一个数组
    本示例演示“正则表达式探测点匹配”和获取输入参数作为一个数组。
    所以，任何类型的重载变量都可以追溯到“一个地方”。

--- src/main/java/com/sun/btrace/samples/ArgArray.java
+++ src/main/java/com/sun/btrace/samples/ArgArray.java
@@ -30,22 +30,32 @@ import com.sun.btrace.AnyType;
 import static com.sun.btrace.BTraceUtils.*;
 
 /**
- * This sample demonstrates regular expression
- * probe matching and getting input arguments
- * as an array - so that any overload variant
- * can be traced in "one place". This example
- * traces any "readXX" method on any class in
- * java.io package. Probed class, method and arg
- * array is printed in the action.
+ * <p>
+ *     本示例演示“正则表达式探测点匹配”和获取输入参数作为一个数组。
+ *     所以，任何类型的重载变量都可以追溯到“一个地方”。
+ *     本示例追踪在java.io包中任何类的"readXXX"方法，
+ *     探测的类、方法和参数数组在追踪行为中被打印。
+ * </p>
+ * This sample demonstrates regular expression probe matching and getting input arguments
+ * as an array - so that any overload variant can be traced in "one place".
+ * This example traces any "readXXX" method on any class in java.io package.
+ * Probed class, method and arg array is printed in the action.
  */
-@BTrace public class ArgArray {
+@BTrace
+public class ArgArray {
+
     @OnMethod(
-        clazz="/java\\.io\\..*/",
-        method="/read.*/"
+        // 多个"类/方法"匹配
+        clazz="/java\\.io\\..*/", // 匹配在"java.io"包中的任何类
+        method="/read.*/" // 匹配任何"readXXX"方法
     )
     public static void anyRead(@ProbeClassName String pcn, @ProbeMethodName String pmn, AnyType[] args) {
+        // @ProbeClassName：表示“探测目标类名”
+        // @ProbeMethodName：表示“探测目标方法名”
+        // AnyType：表示“任何引用类型(对象或数组)”都允许
         println(pcn);
         println(pmn);
-        printArray(args);
+        printArray(args); // 打印“输入参数数组”
     }
+
 }

ef684c7c76feadb460ff8ca89f25b8bf135e7319
Edward Lee <bert825_work@163.com>
2015-06-23 20:49:06 +0800

    [Sample] ThreadCounterBean.java：本示例演示了可以将一个BTrace类暴露为一个JMX MBean
    在连接BTrace程序到目标应用程序后，再连接VisualVM或jconsole或其他JMX客户端到相同的应用。
    
    @Property：使用本注解的“BTrace字段”会作为“动态JMX实体”的属性暴露给外界

--- src/main/java/com/sun/btrace/samples/ThreadCounterBean.java
+++ src/main/java/com/sun/btrace/samples/ThreadCounterBean.java
@@ -29,28 +29,40 @@ import com.sun.btrace.annotations.*;
 import static com.sun.btrace.BTraceUtils.*;
 
 /**
- * This sample demonstrates that you can expose a BTrace
- * class as a JMX MBean. After connecting BTrace to the
- * target application, connect VisualVM or jconsole or 
- * any other JMX client to the same application.
+ * <p>
+ *     本示例演示了可以将一个BTrace类暴露为一个JMX MBean。
+ *     在连接BTrace程序到目标应用程序后，再连接VisualVM或jconsole或其他JMX客户端到相同的应用。
+ * </p>
+ * This sample demonstrates that you can expose a BTrace class as a JMX MBean.
+ * After connecting BTrace to the target application,
+ * connect VisualVM or jconsole or any other JMX client to the same application.
  */ 
-@BTrace public class ThreadCounterBean {
+@BTrace
+public class ThreadCounterBean {
 
-    // @Property makes the count field to be exposed
-    // as an attribute of this MBean.
-    @Property
+    // @Property makes the count field to be exposed as an attribute of this MBean.
+    // @Property使得“count字段”被暴露为本MBean对象的一个属性
+    @Property // 使用本注解的“BTrace字段”会作为“动态JMX实体”的属性暴露给外界
     private static long count;
 
+    /**
+     * 追踪线程的“Thread.start(...)”开始运行行为。
+     *
+     * @param t 新创建的线程
+     */
     @OnMethod(
         clazz="java.lang.Thread",
         method="start"
     ) 
-    public static void onnewThread(@Self Thread t) {
+    public static void onNewThread(@Self Thread t) {
+        // updating counter is easy. Just assign to the static field!
+        // 更新计数器很容易，只需分配给静态字段！
         count++;
     }
 
     @OnTimer(2000) 
-    public static void ontimer() {
+    public static void onTimer() {
         println(count);
     }
+
 }

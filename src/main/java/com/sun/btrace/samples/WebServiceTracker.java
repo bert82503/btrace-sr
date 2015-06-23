8d807e5ab0a0c32d3fa2715ec24cf8b2f0a3412c
Edward Lee <bert825_work@163.com>
2015-06-23 23:22:54 +0800

    [Sample] WebServiceTracker.java：简单的BTrace程序：在“Web服务”被调用时打印类名和方法名，同时打印“服务方法的耗时”
    “Web服务入口点”被注释为@WebService和@WebMethod，我们插入“追踪行为“到每一个使用了这些注解的类和方法。
    这种方式，我们无需知道真实的“Web服务服务实现类名”。

--- src/main/java/com/sun/btrace/samples/WebServiceTracker.java
+++ src/main/java/com/sun/btrace/samples/WebServiceTracker.java
@@ -29,35 +29,49 @@ import static com.sun.btrace.BTraceUtils.*;
 import com.sun.btrace.annotations.*;
 
 /**
- * A simple BTrace program that prints a class name
- * and method name whenever a webservice is called and
- * also prints time taken by service method. WebService 
- * entry points are annotated javax.jws.WebService and 
- * javax.jws.WebMethod. We insert tracing actions into 
- * every class and method annotated by these annotations. 
- * This way we don't need to know actual webservice 
- * implementor class name.
+ * <p>
+ *     简单的BTrace程序：在“Web服务”被调用时打印类名和方法名，同时打印“服务方法的耗时”。
+ *     “Web服务入口点”被注释为@WebService和@WebMethod，我们插入“追踪行为“到每一个使用了这些注解的类和方法。
+ *     这种方式，我们无需知道真实的“Web服务服务实现类名”。
+ * </p>
+ * A simple BTrace program that prints a class name and method name whenever a webservice is called
+ * and also prints time taken by service method.
+ * WebService entry points are annotated javax.jws.WebService and javax.jws.WebMethod.
+ * We insert tracing actions into every class and method annotated by these annotations.
+ * This way we don't need to know actual WebService implement class name.
  */
-@BTrace public class WebServiceTracker {
-   @OnMethod(
-     clazz="@javax.jws.WebService", 
-     method="@javax.jws.WebMethod"
-   )   
-   public static void onWebserviceEntry(@ProbeClassName String pcn, @ProbeMethodName String pmn) {
-       print("entering webservice ");
-       println(Strings.strcat(Strings.strcat(pcn, "."), pmn));
-   }
+@BTrace
+public class WebServiceTracker {
 
-   @OnMethod(
-     clazz="@javax.jws.WebService", 
-     method="@javax.jws.WebMethod",
-     location=@Location(Kind.RETURN)
-   )   
-   public static void onWebserviceReturn(@ProbeClassName String pcn , @ProbeMethodName String pmn, @Duration long d) {
-       print("leaving web service ");
-       println(Strings.strcat(Strings.strcat(pcn, "."), pmn));
-       println(Strings.strcat("Time taken (msec) ", Strings.str(d / 1000)));
-       println("==========================");
-   }
+    /**
+     * 追踪所有“@WebService”注释的类中使用了“@WebMethod”注释的方法。
+     *
+     * @param pcn 探测的类名
+     * @param pmn 探测的方法名
+     */
+    @OnMethod(
+            clazz = "@javax.jws.WebService", // 匹配所有使用“@WebService”注释的类
+            method = "@javax.jws.WebMethod" // 匹配所有使用“@WebMethod”注释的方法
+    )
+    public static void onWebServiceEntry(
+            @ProbeClassName String pcn, @ProbeMethodName String pmn) {
+        print("entering web service "); // 进入“Web服务”
+        println(Strings.strcat(Strings.strcat(pcn, "."), pmn)); // 打印“类名.方法名”信息
+    }
+
+    @OnMethod(
+            clazz = "@javax.jws.WebService", // 匹配所有使用“@WebService”注释的类
+            method = "@javax.jws.WebMethod", // 匹配所有使用“@WebMethod”注释的方法
+            location = @Location(Kind.RETURN) // 插入到方法返回时
+    )
+    public static void onWebServiceReturn(
+            @ProbeClassName String pcn, @ProbeMethodName String pmn,
+            @Duration long d // 方法调用时间
+    ) {
+        print("leaving web service "); // 离开“Web服务”
+        println(Strings.strcat(Strings.strcat(pcn, "."), pmn)); // 打印“类名.方法名”信息
+        println(Strings.strcat("Time taken (msec) ", Strings.str(d / 1000))); // 服务方法耗时毫秒数
+        println("==========================");
+    }
 
 }

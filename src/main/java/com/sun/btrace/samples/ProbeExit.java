85fcc6e9a34a024d8cd48baa34bd398957fc5a2d
Edward Lee <bert825_work@163.com>
2015-06-24 21:58:41 +0800

    [Sample] ProbeExit.java：本程序演示了“@OnExit探测点”
    当一些“BTrace行为方法”调用“exit(int)”内建方法时，被“@OnExit”注释的方法会被调用。
    在这个方法中，“BTrace脚本”打印了“追踪/清理”的概要信息。

--- src/main/java/com/sun/btrace/samples/ProbeExit.java
+++ src/main/java/com/sun/btrace/samples/ProbeExit.java
@@ -29,35 +29,43 @@ import com.sun.btrace.annotations.*;
 import static com.sun.btrace.BTraceUtils.*;
 
 /**
+ * <p>
+ *     本程序演示了“@OnExit探测点”。
+ *     当一些“BTrace行为方法”调用“exit(int)”内建方法时，被“@OnExit”注释的方法会被调用。
+ *     在这个方法中，“BTrace脚本”打印了“追踪/清理”的概要信息。
+ * </p>
  * This program demonstrates OnExit probe.
- * When some BTrace action method calls "exit(int)"
- * built-in function, method annotated by @OnExit
- * (if found) is called. In this method, BTrace script
- * print summary information of tracing and/or do clean-up.
+ * When some BTrace action method calls "exit(int)" built-in function,
+ * method annotated by @OnExit (if found) is called.
+ * In this method, BTrace script print summary information of tracing and/or do clean-up.
  */
 
-@BTrace public class ProbeExit {
-    private static volatile int i;
+@BTrace
+public class ProbeExit {
 
-    // @OnExit is called when some BTrace method
-    // calls exit(int) method
+    private static volatile int timeCounter;
+
+    // @OnExit is called when some BTrace method calls exit(int) method
+    // 当一些BTrace方法调用“exit(int)”方法时，被“@OnExit”注释的方法会被调用
     @OnExit
-    public static void onexit(int code) {
-        println("BTrace program exits!");
+    public static void onExit(int code) {
+        println("BTrace program exits!"); // 退出“BTrace程序”
     }
 
-    // We just put @OnTimer probe and exit BTrace
-    // program when the count reaches 5.
-
+    // We just put @OnTimer probe and exit BTrace program when the count reaches 5.
+    // 插入“@OnTimer探测点”，并在计数器到达5次时退出“BTrace程序”
     @OnTimer(1000)
-    public static void ontime() {
+    public static void onTime() {
         println("hello");
-        i++;
-        if (i == 5) {
+        timeCounter++;
+        if (timeCounter == 5) {
             // note that this exits the BTrace client
-            // and not the traced program (which would
-            // be a destructive action!).
-            Sys.exit(0);
+            // and not the traced program (which would be a destructive action!).
+            // 注意：这将退出“BTrace客户端”，而不是“追踪的程序”！
+            // Sys: Wraps the OS related BTrace utility methods
+            // Sys：包装与OS相关的BTrace辅助方法
+            Sys.exit(0); // Exits the BTrace session (退出“BTrace会话”)
         }
     }
+
 }

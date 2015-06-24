52cc7d176054a894c937f8e0d33efa31a7b64a27
Edward Lee <bert825_work@163.com>
2015-06-24 19:47:00 +0800

    [Sample] URLTracker.java：本示例打印每次“URL.openConnection(...)”成功返回的行为
    此外，在DTrace可用的平台上，它会运行“jurls.d”DTrace脚本文件，该脚本会收集通过“btrace:::event探测点”访问的URL请求历史记录。
    通过本BTrace程序，我们打开了“DTrace探测点”（调用dtraceProbe(...)方法）。
    
    注意：这可能与直接使用BTrace程序本身实现历史记录的功能很相似（见 Histogram.java）。
    但是，本示例展示了“DTrace/BTrace”集成的功能。
    在程序退出时，所有“DTrace”脚本会聚合到BTrace进行打印（不会显示地调用DTrace的打印功能）。

--- src/main/java/com/sun/btrace/samples/URLTracker.java
+++ src/main/java/com/sun/btrace/samples/URLTracker.java
@@ -27,26 +27,38 @@ package com.sun.btrace.samples;
 
 import com.sun.btrace.annotations.*;
 import static com.sun.btrace.BTraceUtils.*;
-import static com.sun.btrace.BTraceUtils.Strings.*;
 import java.net.*;
 
-/*
- * This sample prints every Java URL openURL and
- * openConnection (successful) attempts. In addition,
- * on platforms where DTrace is available, it runs
- * the D-script jurls.d -- which collects a histogram
- * of URL accesses by a btrace:::event probe. From this
- * BTrace program we raise that DTrace probe (dtraceProbe
- * call). Note that it is possible to do similar histogram
- * in BTrace itself (see Histogram.java). But, this sample
- * shows DTrace/BTrace integration as well. On exit, all
- * DTrace aggregates are printed by BTrace (i.e., the ones
- * that are not explicitly printed by DTrace printa call).
+/**
+ * <p>
+ *     本示例打印每次“URL.openConnection(...)”成功返回的行为。
+ *     此外，在DTrace可用的平台上，它会运行“jurls.d”DTrace脚本文件，
+ *     该脚本会收集通过“btrace:::event探测点”访问的URL请求历史记录。
+ *     通过本BTrace程序，我们打开了“DTrace探测点”（调用dtraceProbe(...)方法）。
+ *
+ *     注意：这可能与直接使用BTrace程序本身实现历史记录的功能很相似（见 Histogram.java）。
+ *     但是，本示例展示了“DTrace/BTrace”集成的功能。
+ *     在程序退出时，所有“DTrace”脚本会聚合到BTrace进行打印（不会显示地调用DTrace的打印功能）。
+ * </p>
+ * This sample prints every Java URL openURL and openConnection (successful) attempts.
+ * In addition, on platforms where DTrace is available, it runs
+ * the D-script jurls.d -- which collects a histogram of URL accesses by a btrace:::event probe.
+ * From this BTrace program we raise that DTrace probe (dtraceProbe call).
+ *
+ * Note that it is possible to do similar histogram in BTrace itself (see Histogram.java).
+ * But, this sample shows DTrace/BTrace integration as well.
+ * On exit, all DTrace aggregates are printed by BTrace (i.e., the ones
+ * that are not explicitly printed by DTrace print call).
  */
-@DTraceRef("jurls.d")
-@BTrace public class URLTracker {
-    @TLS private static URL url;
+@DTraceRef("jurls.d") // 关联一个“DTrace脚本文件（D-script）”
+@BTrace
+public class URLTracker {
 
+    @TLS private static URL url; // 请求URL
+
+    /**
+     * 追踪URL打开请求连接的“URL.openConnection()”行为。
+     */
     @OnMethod(
         clazz="java.net.URL",
         method="openConnection"
@@ -55,6 +67,9 @@ import java.net.*;
         url = self;
     }
 
+    /**
+     * 追踪URL打开代理请求连接的“URL.openConnection(Proxy proxy)”行为。
+     */
     @OnMethod(
         clazz="java.net.URL",
         method="openConnection"
@@ -66,13 +81,17 @@ import java.net.*;
     @OnMethod(
         clazz="java.net.URL",
         method="openConnection",
-        location=@Location(Kind.RETURN)
+        location=@Location(Kind.RETURN) // 连接成功后返回
     )
-    public static void openURL() {
+    public static void openURLReturn() {
         if (url != null) {
             println(Strings.strcat("open ", Strings.str(url)));
+            // D：Wraps the dtrace related BTrace utility methods
+            // D：包装与“BTrace”相关的“DTrace”辅助方法
+            // D.probe(...)：BTrace到DTrace通信通道
             D.probe("java-url-open", Strings.str(url));
             url = null;
         }
     }
+
 }
--- src/main/java/com/sun/btrace/samples/jurls.d
+++ src/main/java/com/sun/btrace/samples/jurls.d
@@ -1,12 +1,14 @@
 
 /*
+ * 只要“btrace:::event”探测点作为第一个参数被“java-url-open”触发时，
+ * 本“DTrace脚本文件”都会维护一个聚合功能。
+ *
  * This D-script maintains a aggregation whenever
- * btrace:::event probe is raised with "java-url-open"
- * as first argument.
+ * btrace:::event probe is raised with "java-url-open" as first argument.
  */
+
 btrace$target:::event
 / copyinstr(arg0) == "java-url-open" /
 {
     @[copyinstr(arg1)] = count();
 }
-
